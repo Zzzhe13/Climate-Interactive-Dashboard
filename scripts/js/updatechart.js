ee15ae49a941655673a0a5d0540f7d87311ed195
Zzzhe <106586475+Zzzhe13@users.noreply.github.com>
2023-12-17 13:34:18 +0800

    Delete scripts/js directory

--- scripts/js/updatechart.js
+++
@@ -1,6 +0,0 @@
-function updateCharts() {
-    // Use the selectedYear variable to update charts in the center
-    // Example: Draw charts for the selected year
-    console.log("Updating charts for year:", selectedYear);
-    // Add your chart update logic here
-}
